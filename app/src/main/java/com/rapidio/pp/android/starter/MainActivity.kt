adaa192edeebf956105dc0f82c2aa62dd16211f1
Matt-Hurd <matt.hurd@instacart.com>
2017-07-29 20:48:33 -0700

    log location changes

--- app/src/main/java/com/rapidio/pp/android/starter/MainActivity.kt
+++ app/src/main/java/com/rapidio/pp/android/starter/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.rapidio.pp.android.starter
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
-import io.reactivex.disposables.CompositeDisposable
+import com.google.android.gms.location.LocationRequest
+import com.patloew.rxlocation.RxLocation
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 import io.rapid.Rapid
@@ -17,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     @BindView(R.id.pp_text_1) lateinit var textView1: TextView
     @BindView(R.id.pp_button_1) lateinit var button1: Button
 
-    internal var cd = CompositeDisposable()
+    var rxLocation: RxLocation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +34,16 @@ class MainActivity : AppCompatActivity() {
         ButterKnife.bind(this)
         subscribeToRapid()
         button1.setOnClickListener { testAddItem() }
+        rxLocation = RxLocation(this)
+        requestLocationPermissions()
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cd.clear()
     }
 
     fun testAddItem() {
@@ -47,4 +58,52 @@ class MainActivity : AppCompatActivity() {
                 })
     }
 
+    fun requestLocationPermissions() {
+        val neededPermissions = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+        var hasAllPermissions = true
+        neededPermissions.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        neededPermissions,
+                        MY_PERMISSIONS_REQUEST_CALLBACK)
+                hasAllPermissions = false
+            }
+        }
+        if (hasAllPermissions) {
+            onLocationPermissionGranted()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun onLocationPermissionGranted() {
+        val locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(1000)
+
+        rxLocation?.let {
+            it.location().updates(locationRequest)
+                    .flatMap { location -> it.geocoding().fromLocation(location).toObservable() }
+                    .subscribe({
+                        Log.d("locations", "ayyy")
+                    })
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CALLBACK -> {
+                if (grantResults.isNotEmpty()) {
+                    onLocationPermissionGranted()
+                } else {
+                    requestLocationPermissions()
+                }
+                return
+            }
+        }
+    }
+
+    companion object {
+        val MY_PERMISSIONS_REQUEST_CALLBACK = 1234
+    }
 }
