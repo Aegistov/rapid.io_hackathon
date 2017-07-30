5cddaf33131af2588a70d51fc75c855229c0186b
Matt-Hurd <matt.hurd@instacart.com>
2017-07-30 11:47:35 -0700

    enable party creation

--- app/src/main/java/com/rapidio/pp/android/starter/SyncMember.kt
+++ app/src/main/java/com/rapidio/pp/android/starter/SyncMember.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sync_member.*
 
 class SyncMember : AppCompatActivity() {
@@ -11,7 +12,8 @@ class SyncMember : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_create -> {
-//                message.setText(R.string.create_party)
+                val myToast = Toast.makeText(this, "Party Created!", Toast.LENGTH_SHORT)
+                myToast.show()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_ready -> {
